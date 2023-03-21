cb37efd5a1eae804316101e5aa7e207bc689370a
Rizki Budi Prasetyo <rizkibudi513@gmail.com>
2023-03-21 13:36:40 +0700

    feat: main page

---
+++ src/constants/social-links.ts
@@ -0,0 +1,46 @@
+export default [
+  {
+    url: "https://saweria.co/rizbud",
+    title: "Saweria",
+    icon: "/svg/saweria.png",
+    class: "bg-purple-700",
+    iconClass: "scale-75",
+  },
+  {
+    url: "https://linkedin.com/in/rizkibudi",
+    title: "LinkedIn",
+    icon: "/svg/linkedin.svg",
+  },
+  {
+    url: "https://github.com/rizbud",
+    title: "Github",
+    icon: "/svg/github.svg",
+    class: "border border-white border-opacity-25",
+  },
+  {
+    url: "https://twitter.com/ri_zbud",
+    title: "Twitter",
+    icon: "/svg/twitter.svg",
+  },
+  {
+    url: "https://instagram.com/rizb.ud",
+    title: "Instagram",
+    icon: "/svg/instagram.svg",
+  },
+  {
+    url: "https://facebook.com/rizbud16",
+    title: "Facebook",
+    icon: "/svg/facebook.svg",
+  },
+  {
+    url: "https://tiktok.com/@rizb.ud",
+    title: "TikTok",
+    icon: "/svg/tiktok.svg",
+    class: "border border-white border-opacity-25",
+  },
+  {
+    url: "https://steamcommunity.com/id/moebudd",
+    title: "Steam",
+    icon: "/svg/steam.svg",
+  },
+];
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        kaushan: ["KaushanScript", ...defaultTheme.fontFamily.sans],
+        lato: ["Lato", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
